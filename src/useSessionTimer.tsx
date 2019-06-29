211235e5f220d7452c3c0ea401130272e66f3017
Jonathan Borg <jonathan@x3m.io>
2019-06-29 15:51:16 +0200

    fix: add return types

--- src/useSessionTimer.tsx
+++ src/useSessionTimer.tsx
@@ -1,49 +1,60 @@
 import { useEffect, useState } from "react";
 
-
-function useSetTimeout(callback: Function, timeout: number, reset: boolean, start: boolean): void {
-    useEffect(() => {
-        if (start) {
-            let id = setTimeout(callback, timeout);
-            return () => {
-                clearTimeout(id);
-            };
-        }
-    }, [reset, start]);
+function useSetTimeout(
+  callback: Function,
+  timeout: number,
+  reset: boolean,
+  start: boolean
+): void {
+  useEffect(() => {
+    if (start) {
+      let id = setTimeout(callback, timeout);
+      return () => {
+        clearTimeout(id);
+      };
+    }
+  }, [reset, start]);
 }
+
 type useSessionTimerProps = {
-    warnAfter: number,
-    expireAfter: number,
-    onWarning: Function,
-    onExpire: Function,
-    onReset: Function
-}
+  warnAfter: number;
+  expireAfter: number;
+  onWarning: Function;
+  onExpire: Function;
+  onReset: Function;
+};
+
+type useSessionTimerReturns = {
+  start: Function;
+  stop: Function;
+  reset: Function;
+};
 export default function useSessionTimer({
-    warnAfter,
-    expireAfter,
-    onWarning,
-    onExpire,
-    onReset
-}: useSessionTimerProps) {
-    const [start, setStart] = useState(false);
-    const [reset, setReset] = useState(false);
+  warnAfter,
+  expireAfter,
+  onWarning,
+  onExpire,
+  onReset
+}: useSessionTimerProps): useSessionTimerReturns {
+  const [start, setStart] = useState(false);
+  const [reset, setReset] = useState(false);
 
-    useSetTimeout(onWarning, warnAfter, reset, start);
-    useSetTimeout(expireProxyfn, expireAfter, reset, start);
+  useSetTimeout(onWarning, warnAfter, reset, start);
+  useSetTimeout(expireProxyfn, expireAfter, reset, start);
 
-    function expireProxyfn() {
-        setStart(false);
-        onExpire();
-    }
-    function handleStart() {
-        setStart(true);
-    }
-    function handleStop() {
-        setStart(false);
-    }
-    function handleReset() {
-        onReset();
-        setReset(!reset);
-    }
-    return { start: handleStart, stop: handleStop, reset: handleReset };
+  function expireProxyfn() {
+    setStart(false);
+    onExpire();
+  }
+  function handleStart() {
+    setStart(true);
+  }
+  function handleStop() {
+    setStart(false);
+  }
+  function handleReset() {
+    onReset();
+    setReset(!reset);
+  }
+  return { start: handleStart, stop: handleStop, reset: handleReset };
 }
